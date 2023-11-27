75f8e50c58f8d887e1377a3f50f1a55e5c0af985
oneill-aoifesarah <>
2023-11-27 03:01:48 +0000

    feat:add get_word function to get word based on difficulty

--- hangman.py
+++ hangman.py
@@ -1,3 +1,15 @@
 # Import and enable random function
 import random
 from words import word_list_easy, word_list_medium, word_list_hard
+
+
+# Function to get a word based on difficulty
+def get_word(difficulty):
+    if difficulty == 'easy':
+        return random.choice(word_list_easy)
+    elif difficulty == 'medium':
+        return random.choice(word_list_medium)
+    elif difficulty == 'hard':
+        return random.choice(word_list_hard)
+    else:
+        return None
